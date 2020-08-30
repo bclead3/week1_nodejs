b56a6068b7c5955eca3530614bc2ad0b33012a7d
Ben Leadholm <leadh009@umn.edu>
2020-08-30 14:06:12 -0500

    Express Simple REST

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require('express'),
+      morgan = require('morgan'),
+      bodyParser = require('body-parser'),
+      http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    // console.log('hostname:' + hostname);
-    // console.log(`port:${port}`);
-    console.log('Request for ' + req.url + ' by method ' + req.method);
+const app = express();
 
-  if (req.method == 'GET') {
-    var fileUrl;
-    if (req.url == '/') fileUrl = '/index.html';
-    else fileUrl = req.url;
+app.use(morgan('dev'));
 
-    var filePath = path.resolve('./public'+fileUrl);
-    const fileExt = path.extname(filePath);
-    if (fileExt == '.html') {
-      fs.exists(filePath, (exists) => {
-        if (!exists) {
-          res.statusCode = 404;
-          res.setHeader('Content-Type', 'text/html');
-          res.end('<html><body><h1>Error 404: ' + fileUrl + 
-                      ' not found</h1></body></html>');
-          return;
-        }
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream(filePath).pipe(res);
-      });
-    }
-    else {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/html');
-      res.end('<html><body><h1>Error 404: ' + fileUrl + 
-              ' not a HTML file</h1></body></html>');
-    }
-  }
-  else {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/html');
-      res.end('<html><body><h1>Error 404: ' + req.method + 
-              ' not supported</h1></body></html>');
-  }
-})
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+// app.use((req, res, next) => {
+//   console.log(req.headers);
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+// });
+
+app.all('/dishes', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+});
+
+app.get('/dishes', (req, res, next) => {
+  res.end('Will send the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+  console.log(req.body)
+  res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+  res.end('Deleting all dishes');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+  res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supportd on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
